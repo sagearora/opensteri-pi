8acad1261a81edfbd2f37f7826523515c195d28e
Saj Arora <saj@aroradental.com>
2024-12-08 09:24:09 -0500

    add cors

--- printer/src/server.ts
+++ printer/src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import { checkStatus, printLabels } from './printHandler';
+import cors from 'cors'
 
 const app = express();
 const port = 8080;
 
 app.use(express.json());
+app.use(cors)
 
 app.get('/', (_, res) => {
     res.json({
